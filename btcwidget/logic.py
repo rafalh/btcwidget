7212a50aaf1e60ccb121b47ac2d6c77147c33876
Rafał Harabień <rafalh1992@o2.pl>
2017-09-01 14:48:17 +0200

    Use more threads for fetching data - faster start.

--- btcwidget/logic.py
+++ btcwidget/logic.py
@@ -16,6 +16,8 @@ class UpdateThread(threading.Thread):
         self._last_graph_update = 0
         self._graph_data_dict = {}
         self._last_ticer = {}
+        self._ticker_threads = {}
+        self._graph_threads = {}
         config.register_change_callback(self.clear)
 
     def run(self):
@@ -26,10 +28,10 @@ class UpdateThread(threading.Thread):
 
     def _update_tickers(self):
         for i, market_config in enumerate(config['markets']):
-            try:
-                self._update_market_ticker(i)
-            except Exception as e:
-                print('Failed to update ticker data for {}: {}'.format(market_config['exchange'], e), file=sys.stderr)
+            if i not in self._ticker_threads:
+                thread = threading.Thread(target=self._fetch_market_ticker, args=(i,), daemon=True)
+                self._ticker_threads[i] = thread
+                thread.start()
 
     def _update_graph(self):
         now = time.time()
@@ -40,49 +42,66 @@ class UpdateThread(threading.Thread):
         for i, market_config in enumerate(config['markets']):
             if market_config['graph']:
                 if update_graph:
-                    try:
-                        self._update_market_graph_data(i)
-                    except Exception as e:
-                        print('Failed to update graph data for {}: {}'.format(market_config['exchange'], e),
-                              file=sys.stderr)
+                    thread = threading.Thread(target=self._fetch_market_graph_data, args=(i,), daemon=True)
+                    self._graph_threads[i] = thread
+                    thread.start()
 
-                if i in self._graph_data_dict:
-                    graph_data = self._graph_data_dict[i]
-                    GObject.idle_add(self._main_win.set_graph_data, i, graph_data)
-
-    def _update_market_ticker(self, market_index):
+    def _fetch_market_ticker(self, market_index):
         now = time.time()
+
         market_config = config['markets'][market_index]
         exchange, market = market_config['exchange'], market_config['market']
         provider = btcwidget.exchanges.factory.get(exchange)
-        price = provider.ticker(market)
-        price_str = btcwidget.currency.service.format_price(price, market[3:])
-        print('{} {} ticker: {}'.format(provider.get_name(), market, price_str))
-        GObject.idle_add(self._main_win.set_current_price, market_index, price_str)
+        try:
+            price = provider.ticker(market)
+        except Exception as e:
+            print('Failed to update ticker data for {}: {}'.format(market_config['exchange'], e), file=sys.stderr)
+            price = None
 
-        self._check_alarms(exchange, market, price)
+        if price:
+            price_str = btcwidget.currency.service.format_price(price, market[3:])
+            print('{} {} ticker: {}'.format(provider.get_name(), market, price_str))
+            GObject.idle_add(self._main_win.set_current_price, market_index, price_str)
 
-        self._last_ticer[market_index] = {
-            'time': now,
-            'open': price,
-            'close': price,
-        }
-        if market_index in self._graph_data_dict:
-            graph_data = self._graph_data_dict[market_index]
-            graph_data.append(self._last_ticer[market_index])
-            graph_since_time = now - config['graph_period_sec']
-            self._graph_data_dict[market_index] = [t for t in graph_data if t['time'] > graph_since_time]
+            self._check_alarms(exchange, market, price)
 
-    def _update_market_graph_data(self, market_index):
-        now = time.time()
+            self._last_ticer[market_index] = {
+                'time': now,
+                'open': price,
+                'close': price,
+            }
+            if market_index in self._graph_data_dict:
+                graph_data = self._graph_data_dict[market_index]
+                graph_data.append(self._last_ticer[market_index])
+                self._update_market_graph(market_index)
+
+        self._ticker_threads.pop(market_index, None)
+
+    def _fetch_market_graph_data(self, market_index):
+
         market_config = config['markets'][market_index]
         exchange, market = market_config['exchange'], market_config['market']
         provider = btcwidget.exchanges.factory.get(exchange)
-        graph_data = provider.graph(market, config['graph_period_sec'], config['graph_res'])
-        if market_index in self._last_ticer:
-            graph_data.append(self._last_ticer[market_index])
+
+        try:
+            graph_data = provider.graph(market, config['graph_period_sec'], config['graph_res'])
+        except Exception as e:
+            print('Failed to update graph data for {}: {}'.format(market_config['exchange'], e), file=sys.stderr)
+            graph_data = None
+
+        if graph_data:
+            if market_index in self._last_ticer:
+                graph_data.append(self._last_ticer[market_index])
+            self._graph_data_dict[market_index] = graph_data
+            self._update_market_graph(market_index)
+
+        self._graph_threads.pop(market_index, None)
+
+    def _update_market_graph(self, i):
+        now = time.time()
+        graph_data = self._graph_data_dict[i]
         graph_data = [t for t in graph_data if t['time'] > now - config['graph_period_sec']]
-        self._graph_data_dict[market_index] = graph_data
+        GObject.idle_add(self._main_win.set_graph_data, i, graph_data)
 
     def clear(self):
         self._last_graph_update = 0
